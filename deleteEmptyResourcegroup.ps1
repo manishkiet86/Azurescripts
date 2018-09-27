af712b631b31557a3a9e47f9de151e5fbbe2dc5f
manishkiet86 <manishkiet86@users.noreply.github.com>
2018-09-28 03:41:37 +0530

    First Commit
    First Commit

---
+++ deleteEmptyResourcegroup.ps1
@@ -0,0 +1,20 @@
+﻿#==================================================
+#This PS script is used to remove Empty resource groups
+#==================================================
+
+#Login-AzureRmAccount
+$resourcegroup = Get-AzureRmResourceGroup
+
+#Set-ExecutionPolicy RemoteSigned
+
+
+foreach($existingresource in $resourcegroup)
+{
+$res= Get-AzureRmResource -ResourceGroupName $existingresource.ResourceGroupName
+Write-Host( $existingresource.ResourceGroupName + ")->Count:" + $res.length  + " (" + $existingresource.ResourceId +")" )
+if($res.Length -eq 0)
+    {
+         Remove-AzureRmResourceGroup -Name $existingresource.ResourceGroupName -Force
+        Write-Host("deleted:" + $existingresource.ResourceGroupName )
+    }
+} 
